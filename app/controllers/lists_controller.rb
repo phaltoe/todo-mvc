42f0525b4a3fcc47065b9d343edc482a9edbd62a
phaltoe <reverse9@live.com>
2016-07-12 15:42:04 -0300

    stubbed out creat form on index - need to fix display show view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,22 @@
 class ListsController < ApplicationController
 
   def index
+    @list = List.new
     @lists = List.all
   end
 
-  def show 
+  def create
+    @list = List.new
+    @list.name = params[:name]
+    if @list.valid?
+      @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :index
+    end
+  end
+
+    def show 
     @list = List.find(params[:id])
   end
 end
