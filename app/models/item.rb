15ec4089b3e19aebc2b7eed6c2b07ab95f832ed9
phaltoe <reverse9@live.com>
2016-08-09 09:47:07 -0300

    added JS submit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ActiveRecord::Base
   belongs_to :list
   validates_presence_of :description
+
+  STATUS = {
+    :incomplete => 0,
+    :complete => 1
+  }
+
+  def complete?
+    self.status == STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status == STATUS[:incomplete]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712211008) do
+ActiveRecord::Schema.define(version: 20160808222801) do
 
   create_table "items", force: :cascade do |t|
     t.string   "description"
     t.integer  "list_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "status",      default: 0
   end
 
   create_table "lists", force: :cascade do |t|
