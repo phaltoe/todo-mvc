b678fa7bd2df3c42f432ef986e8996f61386b330
phaltoe <reverse9@live.com>
2016-08-15 13:14:58 -0300

    created users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808222801) do
+ActiveRecord::Schema.define(version: 20160815160650) do
 
   create_table "items", force: :cascade do |t|
     t.string   "description"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160808222801) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
