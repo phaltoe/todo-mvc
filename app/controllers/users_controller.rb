47f3799d49bd87525b84ae0ae3be586c8909dc5b
phaltoe <reverse9@live.com>
2016-08-15 15:28:46 -0300

    added uniqueness/presence validation for user[email]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    # if !@user.nil? && !User.exists?(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
