095c64851581567c3484e1e0a8a4fba76ad86d52
phaltoe <reverse9@live.com>
2016-08-16 07:52:14 -0300

    current user done/ will insert password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,18 @@ class ApplicationController < ActionController::Base
   end 
 
   def logged_in?
-    session[:user_id]
+    !!current_user
   end 
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  #METHODS YOU BUILD IN THE CONTROLLER DO NOT PERMEATE TO THE ACTIONVIEW PART OF YOUR CODE,
+  #UNLESS YOU EXPLICITLY YOU CAN'T CALL THIS ON YOUR HTML.ERB
+
+  #THIS IS A HELPER METHOD, PLEASE LET ME USE THIS IN MY VIEW:
+
+  helper_method :current_user 
 
 end
