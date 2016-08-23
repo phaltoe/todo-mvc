d02f9b3d57767c7b7448c1201ab2f17a230a2750
phaltoe <reverse9@live.com>
2016-08-23 10:50:14 -0300

    working now it was a bug from github not passing the email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.find_or_create_by_omniauth(auth_hash)
-    self.where(email: auth_hash["info"]["uid"]).first_or_create do |user|
+    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
       user.password = SecureRandom.hex
     end
   end
